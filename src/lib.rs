89aecd6f23665c5f9c9acf83ca9667fbd77c6ad8
Floris Heinen <florisheinen1@gmail.com>
2025-11-28 01:53:10 +0100

    Add file comments to src

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! This crate provides a rate limiter that follows a token-bucket approach,
+//! while taking the runtime of the task into account.
+//! Convenient for calling strict rate-limited APIs, Ritlers can be used
+//! to ensure that even when routing inconsistencies happen, your requests
+//! will never accidentally arrive simultaneously above the rate limit.
+//! This is done by waiting for the task to finish(wait for a response) before
+//! it starts to schedule the next task.
 use std::{
 	future::Future,
 	pin::Pin,
